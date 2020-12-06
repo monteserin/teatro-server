3d3d0e5a3f2a046478c2a4ffed406b33bf1ba0ca
Pablo Monteserín <paramonty@gmail.com>
2020-12-06 16:01:15 +0100

    subida

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 
 export default {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'teatro-server',
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'b84312d7539c93',
+    password: '75989781',
+    database: 'heroku_36dfd48c92e173b',
     connectionLimit: 10,
 };
