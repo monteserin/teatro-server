bc290738cdb56b3c20a0d602ee0d935596b9d018
Pablo Monteserín <paramonty@gmail.com>
2020-12-06 15:53:06 +0100

    subida

--- build/bundle.js
+++ build/bundle.js
@@ -245,6 +245,9 @@ __webpack_require__.r(__webpack_exports__);
   app.use('/plays', _plays__WEBPACK_IMPORTED_MODULE_2__["default"]); // Private
 
   app.use('/users', _users__WEBPACK_IMPORTED_MODULE_0__["default"]);
+  app.use('/', function (req, res) {
+    return res.send("Working like a charm \u0669(^\u203F^)\u06F6");
+  });
 });
 
 /***/ }),
@@ -304,10 +307,10 @@ router.post("/create", Object(_Middlwares_error_handler__WEBPACK_IMPORTED_MODULE
             mail = req.body.mail;
             pass = req.body.pass;
             _context2.next = 4;
-            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["createUser"](mail, pass);
+            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["createPlay"](mail, pass);
 
           case 4:
-            res.send('Usuario creado con éxito');
+            res.send('Play creado con éxito');
 
           case 5:
           case "end":
@@ -330,10 +333,10 @@ router["delete"]("/:id", _Middlwares_restricted_access__WEBPACK_IMPORTED_MODULE_
           case 0:
             id = req.params.id;
             _context3.next = 3;
-            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["deleteUser"](id);
+            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["deletePlay"](id);
 
           case 3:
-            res.send("User id: ".concat(id, " deleted"));
+            res.send("Play id: ".concat(id, " deleted"));
 
           case 4:
           case "end":
@@ -356,10 +359,10 @@ router.patch("/:id", _Middlwares_restricted_access__WEBPACK_IMPORTED_MODULE_5__[
           case 0:
             id = req.params.id, body = req.body;
             _context4.next = 3;
-            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["updateUser"](id, body);
+            return _Models_plays__WEBPACK_IMPORTED_MODULE_3__["updatePlay"](id, body);
 
           case 3:
-            res.send("User id: ".concat(id, " updated"));
+            res.send("Play id: ".concat(id, " updated"));
 
           case 4:
           case "end":
@@ -652,36 +655,65 @@ var Documentation = function Documentation(app) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! express */ "express");
-/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! http */ "http");
-/* harmony import */ var http__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(http__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _middlewares__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./middlewares */ "./src/middlewares/index.js");
-/* harmony import */ var _controllers__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./controllers */ "./src/controllers/index.js");
-/* harmony import */ var _documentation__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./documentation */ "./src/documentation/index.js");
-/* harmony import */ var _database__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./database */ "./src/database/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "@babel/runtime/regenerator");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/asyncToGenerator */ "@babel/runtime/helpers/asyncToGenerator");
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! express */ "express");
+/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var http__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! http */ "http");
+/* harmony import */ var http__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(http__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _middlewares__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./middlewares */ "./src/middlewares/index.js");
+/* harmony import */ var _controllers__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./controllers */ "./src/controllers/index.js");
+/* harmony import */ var _documentation__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./documentation */ "./src/documentation/index.js");
+/* harmony import */ var _database__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./database */ "./src/database/index.js");
+/* harmony import */ var _Middlwares_error_handler__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @Middlwares/error-handler */ "./src/middlewares/error-handler.js");
 
 
 
 
 
 
-var app = express__WEBPACK_IMPORTED_MODULE_0___default()();
+
+
+
+var app = express__WEBPACK_IMPORTED_MODULE_2___default()();
+var router = express__WEBPACK_IMPORTED_MODULE_2___default.a.Router();
 /*
 Heroku utiliza la variable de entorno process.env.port para asignar el puerto donde se va ejecutar el servidor,
 por eso la hemos definido en esta plantilla
 */
 
 var port = process.env.PORT || 3005;
-Object(_documentation__WEBPACK_IMPORTED_MODULE_4__["default"])(app);
-Object(_middlewares__WEBPACK_IMPORTED_MODULE_2__["default"])(app);
-Object(_controllers__WEBPACK_IMPORTED_MODULE_3__["default"])(app);
-Object(_database__WEBPACK_IMPORTED_MODULE_5__["default"])(function () {
-  var server = http__WEBPACK_IMPORTED_MODULE_1___default.a.createServer(app);
+Object(_documentation__WEBPACK_IMPORTED_MODULE_6__["default"])(app);
+Object(_middlewares__WEBPACK_IMPORTED_MODULE_4__["default"])(app);
+Object(_controllers__WEBPACK_IMPORTED_MODULE_5__["default"])(app);
+Object(_database__WEBPACK_IMPORTED_MODULE_7__["default"])(function () {
+  var server = http__WEBPACK_IMPORTED_MODULE_3___default.a.createServer(app);
   server.listen(port, function () {
     return console.log("Server listening to http://localhost:".concat(port));
   });
 });
+router.get("/", Object(_Middlwares_error_handler__WEBPACK_IMPORTED_MODULE_8__["asyncHandler"])( /*#__PURE__*/function () {
+  var _ref = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee(req, res) {
+    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            res.send("working \u0669(^\u203F^)\u06F6");
+
+          case 1:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}()));
 
 /***/ }),
 
--- src/controllers/api/plays/index.js
+++ src/controllers/api/plays/index.js
@@ -12,20 +12,20 @@ router.get("/restricted-path", restrictedAccess, asyncHandler(async (req, res) =
 router.post("/create", asyncHandler(async (req, res) => {
   const mail = req.body.mail;
   const pass = req.body.pass;
-  await PlaysModel.createUser(mail, pass)
-  res.send('Usuario creado con éxito');
+  await PlaysModel.createPlay(mail, pass)
+  res.send('Play creado con éxito');
 }));
 
 router.delete("/:id", restrictedAccess, asyncHandler(async (req, res) => {
   const { params: { id } } = req;
-  await PlaysModel.deleteUser(id);
-  res.send(`User id: ${id} deleted`);
+  await PlaysModel.deletePlay(id);
+  res.send(`Play id: ${id} deleted`);
 }));
 
 router.patch("/:id", restrictedAccess, async (req, res) => {
   const { params: { id }, body } = req;
-  await PlaysModel.updateUser(id, body);
-  res.send(`User id: ${id} updated`);
+  await PlaysModel.updatePlay(id, body);
+  res.send(`Play id: ${id} updated`);
 });
 
 
